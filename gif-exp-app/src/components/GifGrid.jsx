a66c05b0dddc3d9d71a0d86a6806e34120a609be
codingjhames <jhames.mejia@unipamplona.edu.co>
2023-02-02 14:07:02 -0500

    className css clases

--- gif-exp-app/src/components/GifGrid.jsx
+++ gif-exp-app/src/components/GifGrid.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { getGifs } from "../helpers/getGifs"
+import { GifItem } from "./GifItem";
 
 
 export const GifGrid = ({category}) => {
@@ -21,18 +22,18 @@ export const GifGrid = ({category}) => {
         <>
             <h3>{category}</h3>
 
-            <ol>
-
+            <div className="card-grid">
                 { 
-                    images.map( ({id,title}) => ( 
+                    images.map( ( image ) => ( 
 
-                        <li key={ id }>{ title }</li>
+                        <GifItem 
+                        key={ image.id } 
+                        { ...image }
+                        />
                                 
                     ) )  
                 }
-                
-            
-            </ol>  
+            </div>  
             
         </>
     )

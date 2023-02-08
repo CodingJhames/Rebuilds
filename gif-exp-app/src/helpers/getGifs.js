1398633aac3707b9e35aff55ffaba87ffa90621a
codingjhames <jhames.mejia@unipamplona.edu.co>
2023-02-07 20:36:31 -0500

    deploy on netlify

--- gif-exp-app/src/helpers/getGifs.js
+++ gif-exp-app/src/helpers/getGifs.js
@@ -2,7 +2,7 @@
 
 export const getGifs = async( category ) => {
 
-    const url = `https:\\api.giphy.com/v1/gifs/search?api_key=BsW6v8H74iBT4mTYmspQQnvmzK5DaDQ6&q=${category}&limit=10`;
+    const url = `http:\\api.giphy.com/v1/gifs/search?api_key=BsW6v8H74iBT4mTYmspQQnvmzK5DaDQ6&q=${category}&limit=10`;
     const resp = await fetch( url );
     const { data } = await resp.json();
     console.log( data );

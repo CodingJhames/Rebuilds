9e80f1ce58bb5417fb50275ab02eb3a3c1f3a854
codingjhames <jhames.mejia@unipamplona.edu.co>
2023-02-22 14:33:30 -0500

    customHook created & expose hook methods

--- hook-app/src/main.jsx
+++ hook-app/src/main.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import { CounterApp } from './useState/CounterApp'
+import { CounterWithCustomHook } from './useState/CounterWithCustomHook'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    < CounterApp />
+    < CounterWithCustomHook />
   </React.StrictMode>,
 )

07cd85ed255376b66494021e9d6c652e87c16b34
codingjhames <jhames.mejia@unipamplona.edu.co>
2023-03-02 15:35:19 -0500

    useEffect precautions

--- hook-app/src/useEffect/Message.jsx
+++ hook-app/src/useEffect/Message.jsx
@@ -5,13 +5,21 @@ import { useEffect } from "react"
 
 export const Message = () => {
 
+
     useEffect(() => {
-      console.log( 'message mounted');
+
+        const onMouseMove = ( { x, y }  ) => {
+            const coords = { x, y};
+            console.log( coords );
+        };
+
+        window.addEventListener( 'mousemove', onMouseMove );
+         
     
       return () => {
-        console.log( 'message unmounted');
+        window.removeEventListener( 'mousemove', onMouseMove );
       }
-    }, [])
+    }, [] );
     
 
 
@@ -22,4 +30,6 @@ export const Message = () => {
     
     </>
   )
+
 }
+

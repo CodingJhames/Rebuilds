d221b7f9a9e6775fb6cb07c9ca1fcc1c5f29692c
codingjhames <jhames.mejia@unipamplona.edu.co>
2023-03-09 14:17:39 -0500

    optimizations to the previous code

--- hook-app/src/examples/MultipleCustomHooks.jsx
+++ hook-app/src/examples/MultipleCustomHooks.jsx
@@ -1,11 +1,16 @@
-import { useCounter } from "../hooks/useCounter";
-import { useFetch } from "../hooks/useFetch"
+
+import { useCounter,useFetch } from "../hooks";
+import { LoadingQuote,Quote } from "./";
+
+
+
 
 
 
 
 export const MultipleCustomHooks = () => {
 
+  
     const { counter, increment, decrement } = useCounter(1);
 
     const {data, isLoading, hasError } = useFetch(`https://api.breakingbadquotes.xyz/v1/quotes/${ counter }`);
@@ -16,21 +21,15 @@ export const MultipleCustomHooks = () => {
     <>
         <h1>Breaking Bad Quotes</h1>
         <hr/>
-
         {
           isLoading
-          ? (
-            <div className="alert alert-info text-center">
-              Loading...
-            </div>
-          )
-          : (
-            <blockquote className="blockquote text-end">
-              <p className="mb-1">{ quote }</p>
-              <footer className="blockquote-footer">{ author }</footer>
-            </blockquote>
-          )
+          ? <LoadingQuote  />
+          :
+          <Quote author={ author } quote={ quote } />
         }
+        
+          
+          
 
         <button 
         className="btn btn-primary" 
